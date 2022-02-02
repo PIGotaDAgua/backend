72090190d9272b65abe8440ede48f1496693b25f
BiancaPinaLins <bianca_palins@outlook.com>
2022-02-02 10:21:40 -0300

    Model e repository

---
+++ pigotadagua/src/main/java/br/org/generation/pigotadagua/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.org.generation.pigotadagua.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.pigotadagua.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+
+}

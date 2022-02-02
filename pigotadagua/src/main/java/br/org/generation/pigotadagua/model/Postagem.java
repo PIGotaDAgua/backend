f40da3e37404b8cf94841a7600409ff79c2b5bf7
nancidama <nanci.damasceno@sumup.com>
2022-02-02 09:16:41 -0300

    Update

--- pigotadagua/src/main/java/br/org/generation/pigotadagua/model/Postagem.java
+++ pigotadagua/src/main/java/br/org/generation/pigotadagua/model/Postagem.java
@@ -2,6 +2,7 @@ package br.org.generation.pigotadagua.model;
 
 import java.time.LocalDateTime;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
